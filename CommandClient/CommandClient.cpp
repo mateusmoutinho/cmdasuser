623ef40598547d7dfc007fbbb6d80ee50f5b0756
Martyn C Brown <martyn.brown@sewtec.co.uk>
2025-03-14 18:03:57 +0000

    Testing changes

--- CommandClient/CommandClient.cpp
+++ CommandClient/CommandClient.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
 #include <asio.hpp>
+#include <thread>
+#include <chrono>
 
 // See <repo>/readme.md
 using asio::ip::tcp;
 
 int main() {
     try {
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+
         asio::io_context io_context;
         tcp::resolver resolver(io_context);
         tcp::resolver::results_type endpoints = resolver.resolve("127.0.0.1", "54000");
