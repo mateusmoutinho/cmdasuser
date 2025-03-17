70737a4f490e21ec2fb23703ebda20b108a00814
Martyn C Brown <martyn.brown@sewtec.co.uk>
2025-03-17 16:24:06 +0000

    Added ConfigService.

---
+++ ConfigService/ConfigService.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+#include <array>
+#include <memory>
+#include <cstdio>
+#include <utility>
+#include <optional>
+#include <asio.hpp>
+#include <CommandLib.h>
+#include <tchar.h>
+#include <windows.h> // wsock2.h is included in CommandLib.h
+
+using namespace CommandLib;
+
+int main() {
+    try {
+        
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
